2780a33b942de2e74183ae8bc6fca84b345a27b0
Anesthesiade4cordas <guilhermecqpro@gmail.com>
2024-06-28 22:03:07 -0300

    first commit

---
+++ eu.js
@@ -0,0 +1,4 @@
+let nome = `guilherme,`
+let idade = `19 anos,`
+let estado_civil = false
+console.log(nome,idade,estado_civil)
---
+++ media.js
@@ -0,0 +1,5 @@
+Number=10
+Number=25
+Number=30
+Number=40
+console.log(10+25+30+40/4)
